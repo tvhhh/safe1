be5f6dc6ae3f1c35b4f826adf041da819f8c62a9
tnhuan1802 <tnhuan1802@gmail.com>
2021-06-16 12:32:53 +0700

    Almost done

--- safe1/src/views/GasConcentration.tsx
+++ safe1/src/views/GasConcentration.tsx
@@ -5,68 +5,257 @@ import {
   Text,
   ScrollView,
   View,
-  Animated
+  Alert
 } from 'react-native';
+import { Svg, Rect, Text as TextSVG }  from 'react-native-svg'
 import LinearGradient from 'react-native-linear-gradient'
-import { LineChart, BarChart } from 'react-native-chart-kit'
+import { LineChart } from 'react-native-chart-kit'
 import Feather from 'react-native-vector-icons/Feather'
 import FontAwesome from 'react-native-vector-icons/FontAwesome'
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons"
+import { connect, ConnectedProps } from 'react-redux';
+import { State } from '@/redux/state';
+import {Device} from '@/models';
 
-const { width } = Dimensions.get('screen');
-const gas_data = [5, 1, 3, 6, 3 ,0, 2]
-const gas_label = ["MON", "TUE", 'WED', "THU", "FRI", "SAT", "SUN"]
-const data = {
-  labels: gas_label,
-  datasets: [
-    {
-      data: gas_data,
-      color: () => 'rgba(250, 218, 94, 0.8)'
-    }               
-  ]
-}
+const { width } = Dimensions.get('screen')
+
+  
+const mapStateToProps = (state: State) => ({
+  currentUser: state.currentUser,
+  defaultBuilding: state.defaultBuilding,
+  buildings: state.buildings
+});
+
+const connector = connect(mapStateToProps);
 
 interface Props {
   navigation: any,
 };
 
-class GasConcentration extends React.Component<Props> {
+interface CustomState {
+  tooltipPos: any
+}
+
+class GasConcentration extends React.Component<Props, CustomState> {
   constructor(props: Props) {
     super(props);
+    this.state = {
+      tooltipPos: {
+        x: 0,
+        y: 0,
+        visible: false,
+        time: "",
+        value: 0
+      }
+    }
   };
+  
+  
+
+  getDevices = (typeDevice: string = "") => {
+    if (typeDevice === "") {
+      return this.props.defaultBuilding.devices
+    } else {
+      return this.props.defaultBuilding.devices.filter(device => device.deviceType == typeDevice)
+    }
+  }
+
+  getGasData = () => {
+    let gas_data = {
+      gas: [],
+      label: []
+    }
+
+    if (this.props.defaultBuilding !== undefined) {
+      const devices = this.getDevices("gas")
+      devices.forEach((device: Device) => {
+        device.data.forEach(data => {
+          const time = new Date(data.time)
+          if (gas_data.gas.length === 6) {
+            gas_data.gas.shift()
+            gas_data.label.shift()
+          }
+          gas_data.gas.push(Number(data.value))
+          gas_data.label.push(time)
+        });
+      });
+    }
+    console.log(gas_data)
+    return gas_data
+  }
+  
+  getHourMinuteSecond = (date: Date) => {
+    const hour = this.formatTime(date.getHours())
+    const minute = this.formatTime(date.getMinutes())
+    const second = this.formatTime(date.getSeconds())
+    return `${hour}:${minute}:${second}`
+  } 
+
+  formatTime = (num: number) => {
+    if (num < 10) {
+      return `0${num}`
+    }
+    return `${num}`
+  }
+
+  checkIfHadDevice = () => {
+    return this.getDevices("gas").length !== 0
+  }
+
+  renderDeviceEmptyBuilding = () => {
+    return(
+      <View style={styles.emptyContainer}>
+        
+        <FontAwesome name="building-o" size={75} color='rgba(255, 255, 255, 0.8)' />
+        <Text style={styles.emptyPrimaryText}>NO BUILDINGS</Text>
+        <Text style={styles.emptySecondaryText}>Please create at least one building!</Text>
+    </View>
+    )  
+  }
+
+  renderEmptyDevice = () => {
+      return(
+        <View style={styles.emptyContainer}>
+          <MaterialCommunityIcons name="leak" size={75} color='rgba(255, 255, 255, 0.8)'/>
+          <Text style={styles.emptyPrimaryText}>NO DEVICES</Text>
+          <Text style={styles.emptySecondaryText}>Please add a temperature device!</Text>
+        </View>
+      )    
+  }
+
+  renderDeviceEmptyData = () => {
+    return(
+      <View style={styles.emptyContainer}>
+        <MaterialCommunityIcons name="database" size={75} color='rgba(255, 255, 255, 0.8)'/>
+        <Text style={styles.emptyPrimaryText}>NO DATA</Text>
+        <Text style={styles.emptySecondaryText}>Gas sensor device has not received any data!</Text>
+      </View>
+    )  
+  }
+
+  renderData = (gas_data: any) => {
+    let data = {
+      labels: gas_data.label,
+      datasets: [
+        {
+          data: gas_data.gas,
+          color: () => 'rgba(250, 218, 94, 0.8)'
+        }               
+      ]
+    }
+    if (this.checkIfHadDevice() === true) {
+      return(
+        <View style={styles.container}>
+          <View style={styles.header}></View>
+          {this.getGasData().gas.length !== 0 ? 
+          <ScrollView style={{flex: 1}} contentContainerStyle={{alignItems:'center'}}>
+            <LineChart
+              onDataPointClick={({ value, index, x, y }) => {
+                // check if we have clicked on the same point again
+                let isSamePoint = (this.state.tooltipPos.x === x
+                                    && this.state.tooltipPos.y ===  y)
+              
+                // if clicked on the same point again toggle visibility
+                // else,render tooltip to new position and update its value
+                isSamePoint ? this.setState((previousState)=> ({
+                                    tooltipPos: {
+                                        ...previousState.tooltipPos, 
+                                        value: value,
+                                        time: new Date(data.labels[index]).toDateString().substring(4),
+                                        visible: !previousState.tooltipPos.visible}
+                                    }))
+                              : 
+                              this.setState({
+                                tooltipPos: {
+                                    x: x, 
+                                    y: y,
+                                    value: value,
+                                    time: new Date(data.labels[index]).toDateString().substring(4),
+                                    visible: true
+                                  }
+                                })
+            }} // end function,
+            decorator={() => {
+              return this.state.tooltipPos.visible ? <View>
+              <Svg>
+                  <Rect 
+                    x={this.state.tooltipPos.x -40}
+                    y={this.state.tooltipPos.y + 10}
+                    width="80"  
+                    height="45"
+                    fill="white"
+                    rx={5}
+                    />
+                  <TextSVG
+                    x={this.state.tooltipPos.x}
+                    y={this.state.tooltipPos.y + 25}
+                    fill="black"
+                    fontSize="12.5"
+                    textAnchor="middle">
+                    {this.state.tooltipPos.time}
+                  </TextSVG>
+                  <TextSVG
+                    x={this.state.tooltipPos.x}
+                    y={this.state.tooltipPos.y + 50}
+                    fill="black"
+                    fontSize="12.5"
+                    textAnchor="middle">
+                    {this.state.tooltipPos.value === 1 ? "Triggered" : "Untriggered"}
+                  </TextSVG>
+              </Svg>
+          </View> : null
+          }}
+              data={data}
+              width={width} // from react-native
+              height={300}
+              yAxisSuffix=""
+              yAxisInterval={1} // optional, defaults to 1
+              chartConfig={{
+                strokeWidth: 3,
+                backgroundGradientFromOpacity: 0,
+                backgroundGradientToOpacity: 0,
+                backgroundGradientTo: "#ffa726",
+                color: () => `rgba(141, 193, 255, 1)`,
+                labelColor: () => 'white',
+                style: {
+                  borderRadius: 16
+                },
+                propsForDots: {
+                  r: "2",
+                },
+                propsForBackgroundLines: {
+                  strokeDasharray: "", // solid background lines with no dashes
+                  opacity: 0.15
+                }
+              }}
+              formatYLabel={(y: string) => {
+                let isInteger = Number.isInteger(Number(y))
+                if (isInteger) {
+                  return Number(y).toString()
+                }
+                return ""
+              }}
+              formatXLabel = {(x: string) => this.getHourMinuteSecond(new Date(x))}
+              fromZero
+              withVerticalLines={false}
+              bezier
+              style={styles.chart}
+            />
+            {gas_data.gas[gas_data.gas.length - 1] !== 1 ? 
+                <MaterialCommunityIcons name = "shield-check" size = {150} color = "white"/>
+              :
+                <MaterialCommunityIcons name = "shield-alert" size = {150} color = "white"/>
+              }
+          </ScrollView>
+          : this.renderDeviceEmptyData()}
+        </View>
+      )
+    }
+    return this.renderEmptyDevice()
+  }
+  
 
   render(){
-    let barChartProps = {
-        data: data,
-        width: width * 0.95, // from react-native
-        height: 400,
-        yAxisInterval: 1, // optional, defaults to 1,
-        yAxisLabel: "",
-        yAxisSuffix: "",
-        chartConfig: {
-          strokeWidth: 3,
-          backgroundGradientFromOpacity: 0,
-          backgroundGradientToOpacity: 0,
-          backgroundGradientTo: "#ffa726",
-          color: () => `rgba(250, 218, 94, 0.8)`,
-          labelColor: () => 'white',
-          style: {
-            borderRadius: 16
-          },
-          propsForDots: {
-            r: "0",
-          },
-          propsForBackgroundLines: {
-            strokeDasharray: "", // solid background lines with no dashes
-            opacity: 0.15
-          }
-        },
-        showBarTops: true,
-        showValuesOnTopOfBars: true,
-        withVerticalLines: false,
-        style: styles.chart
-      }
-
     return (
       <LinearGradient 
         colors={['#4F9FFF', '#002150']} 
@@ -74,40 +263,15 @@ class GasConcentration extends React.Component<Props> {
         start={{x: 0, y: 0}}
         end={{x: 0, y: 0.5}}
       >
-        <View style={styles.container}>
-          <View style={styles.header}></View>
-          <ScrollView style={{flex: 1}} contentContainerStyle={{alignItems:'center'}}>
-            <View style={{paddingRight: 30}}><BarChart {...barChartProps}/></View>
-            <View style={styles.tempContainer}>
-              <MaterialCommunityIcons name = "shield-off" size = {30} color = "white"/>
-              <View style={styles.tempFirstBox}>
-                <Text style={{color: "rgba(255, 255, 255, 0.3)"}}>highest leakage times</Text>
-                <Text style={{color: "rgba(255, 255, 255, 0.3)"}}>on</Text>
-              </View>
-              <View style={styles.tempSecondBox}>
-                <Text style={{color:"white"}}>{Math.max(...gas_data)} times</Text>
-                <Text style={{color:"white"}}>{data.labels[gas_data.indexOf(Math.max(...gas_data))]}</Text>
-              </View>          
-            </View>
-            <View style={styles.tempContainer}>
-              <MaterialCommunityIcons name = "shield-check" size = {30} color = "white"/>
-              <View style={styles.tempFirstBox}>
-                <Text style={{color: "rgba(255, 255, 255, 0.3)"}}>lowest leakage times</Text>
-                <Text style={{color: "rgba(255, 255, 255, 0.3)"}}>on</Text>
-              </View>
-              <View style={styles.tempSecondBox}>
-                <Text style={{color:"white"}}>{Math.min(...gas_data)} times</Text>
-                <Text style={{color:"white"}}>{data.labels[gas_data.indexOf(Math.min(...gas_data))]}</Text>
-              </View>          
-            </View>
-          </ScrollView>
-        </View>
+        {this.props.defaultBuilding !== undefined? 
+          this.renderData(this.getGasData())
+          : this.renderDeviceEmptyBuilding()}
       </LinearGradient>
     )
   }
 }
 
-export default GasConcentration;
+export default connector(GasConcentration);
 
 const styles = StyleSheet.create({
   container: {
@@ -120,23 +284,31 @@ const styles = StyleSheet.create({
   chart: {
     alignItems: 'center'
   },
-  tempContainer: {
+  gasContainer: {
     marginTop: 10,
-    flexDirection: 'row',
     alignItems: 'center',
     justifyContent:'space-evenly',
     borderRadius: 16,
     width: width * 0.8,
-    height: 80,
     backgroundColor: 'rgba(5, 28, 63, 0.5)'
   },
-  tempFirstBox: {
-    height: 70,
-    alignItems: 'center',
-    justifyContent: 'space-around'
+  gasText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white'
   },
-  tempSecondBox: {
-    height: 70,
-    justifyContent: 'space-around'
+  emptyContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  emptyPrimaryText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white'
+  },
+  emptySecondaryText: {
+    fontSize: 18,
+    color: 'rgba(255, 255, 255, 0.6)'
   }
 });
--- safe1/src/views/Temperature.tsx
+++ safe1/src/views/Temperature.tsx
@@ -1,22 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   Dimensions,
   StyleSheet,
   Text,
   ScrollView,
   View,
-  Button,
   Alert
 } from 'react-native';
+import { Tooltip } from 'react-native-elements/dist/tooltip/Tooltip';
+import { Svg, Rect, Text as TextSVG }  from 'react-native-svg'
 import LinearGradient from 'react-native-linear-gradient'
 import { LineChart } from 'react-native-chart-kit'
 import Feather from 'react-native-vector-icons/Feather'
 import FontAwesome from 'react-native-vector-icons/FontAwesome'
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons"
 import { connect, ConnectedProps } from 'react-redux';
 import { State } from '@/redux/state';
+import {Device} from '@/models';
 
-const { width } = Dimensions.get('screen');
+const { width } = Dimensions.get('screen')
 
+  
 const mapStateToProps = (state: State) => ({
   currentUser: state.currentUser,
   defaultBuilding: state.defaultBuilding,
@@ -29,107 +33,214 @@ interface Props {
   navigation: any,
 };
 
-class Temperature extends React.Component<Props> {
+interface CustomState {
+  tooltipPos: any
+}
+
+class Temperature extends React.Component<Props, CustomState> {
   constructor(props: Props) {
     super(props);
     this.state = {
-      defaultBuilding: this.props.defaultBuilding,
-    };
+      tooltipPos: {
+        x: 0,
+        y: 0,
+        visible: false,
+        time: "",
+        value: 0
+      }
+    }
   };
   
+  
+
+  getDevices = (typeDevice: string = "") => {
+    if (typeDevice === "") {
+      return this.props.defaultBuilding.devices
+    } else {
+      return this.props.defaultBuilding.devices.filter(device => device.deviceType == typeDevice)
+    }
+  }
+
   getTempData = () => {
-    const temp_data = {
-      highestTempOfDay: [],
-      lowestTempOfDay: [],
+    let temp_data = {
+      temp: [],
       label: []
     }
-    this.state.defaultBuilding.devices.forEach(device => {
-      var dict: any = {}
-      device.data.forEach(data => {
-        const day = new Date(data.time).toDateString().substring(0,3).toLocaleUpperCase()
-        if (!(day in dict)){
-          dict[day] = []
-        }
-        dict[day].push(Number(data.data.substring(0, 2)))
+    if (this.props.defaultBuilding !== undefined) {
+      const devices = this.getDevices("temperature")
+      devices.forEach((device: Device) => {
+        device.data.forEach(data => {
+          const time = new Date(data.time)
+          if (temp_data.temp.length === 6) {
+            temp_data.temp.shift()
+            temp_data.label.shift()
+          }
+          temp_data.temp.push(Number(data.value.substring(0, 2)))
+          temp_data.label.push(time)
+        });
       });
-      for(const key in dict) {
-        var temp = []
-        temp.push(Math.max(...dict[key]))
-        temp.push(Math.min(...dict[key]))
-        temp_data.highestTempOfDay.push(temp[0])
-        temp_data.lowestTempOfDay.push(temp[1])
-        temp_data.label.push(key)
-      }
-    });
+    }
     return temp_data
   }
   
-  render(){
-    const temp_data = this.getTempData()
-    const data = {
+  getHourMinuteSecond = (date: Date) => {
+    const hour = this.formatTime(date.getHours())
+    const minute = this.formatTime(date.getMinutes())
+    const second = this.formatTime(date.getSeconds())
+    return `${hour}:${minute}:${second}`
+  } 
+
+  formatTime = (num: number) => {
+    if (num < 10) {
+      return `0${num}`
+    }
+    return `${num}`
+  }
+
+  checkIfHadDevice = () => {
+    return this.getDevices("temperature").length !== 0
+  }
+
+  renderDeviceEmptyBuilding = () => {
+    return(
+      <View style={styles.emptyContainer}>
+        
+        <FontAwesome name="building-o" size={75} color='rgba(255, 255, 255, 0.8)' />
+        <Text style={styles.emptyPrimaryText}>NO BUILDINGS</Text>
+        <Text style={styles.emptySecondaryText}>Please create at least one building!</Text>
+    </View>
+    )  
+  }
+
+  renderEmptyDevice = () => {
+      return(
+        <View style={styles.emptyContainer}>
+          <MaterialCommunityIcons name="leak" size={75} color='rgba(255, 255, 255, 0.8)'/>
+          <Text style={styles.emptyPrimaryText}>NO DEVICES</Text>
+          <Text style={styles.emptySecondaryText}>Please add a temperature device!</Text>
+        </View>
+      )    
+  }
+
+  renderDeviceEmptyData = () => {
+    return(
+      <View style={styles.emptyContainer}>
+        <MaterialCommunityIcons name="database" size={75} color='rgba(255, 255, 255, 0.8)'/>
+        <Text style={styles.emptyPrimaryText}>NO DATA</Text>
+        <Text style={styles.emptySecondaryText}>Temperature device has not received any data!</Text>
+      </View>
+    )  
+  }
+
+  renderData = (temp_data: any) => {
+    let data = {
       labels: temp_data.label,
       datasets: [
         {
-          data: temp_data.highestTempOfDay,
+          data: temp_data.temp,
           color: () => 'rgba(250, 218, 94, 0.8)'
-        },
-        {
-          data: temp_data.lowestTempOfDay,
-          color: () => '#87CEFA'
-        }                  
+        }               
       ]
     }
-    let lineChartProps = {
-        data: data,
-        width: width, // from react-native
-        height: 400,
-        yAxisSuffix: "°C",
-        yAxisInterval: 1, // optional, defaults to 1
-        chartConfig: {
-          strokeWidth: 3,
-          backgroundGradientFromOpacity: 0,
-          backgroundGradientToOpacity: 0,
-          backgroundGradientTo: "#ffa726",
-          color: () => `rgba(141, 193, 255, 1)`,
-          labelColor: () => 'white',
-          style: {
-            borderRadius: 16
-          },
-          propsForDots: {
-            r: "0",
-          },
-          propsForBackgroundLines: {
-            strokeDasharray: "", // solid background lines with no dashes
-            opacity: 0.15
-          }
-        },
-        formatYLabel: (y: string) => (Number(y).toString()),
-        fromZero: true,
-        withVerticalLines: false,
-        bezier: true,
-        style: styles.chart
-      }
-
-    return (
-      <LinearGradient 
-        colors={['#4F9FFF', '#002150']} 
-        style={styles.container}
-        start={{x: 0, y: 0}}
-        end={{x: 0, y: 0.5}}
-      >
+    if (this.checkIfHadDevice() === true) {
+      return(
         <View style={styles.container}>
           <View style={styles.header}></View>
+          {this.getTempData().label.length !== 0 ? 
           <ScrollView style={{flex: 1}} contentContainerStyle={{alignItems:'center'}}>
-            <LineChart {...lineChartProps}/>
+            <LineChart
+              onDataPointClick={({ value, index, x, y }) => {
+                // check if we have clicked on the same point again
+                let isSamePoint = (this.state.tooltipPos.x === x
+                                    && this.state.tooltipPos.y ===  y)
+              
+                // if clicked on the same point again toggle visibility
+                // else,render tooltip to new position and update its value
+                isSamePoint ? this.setState((previousState)=> ({
+                                    tooltipPos: {
+                                        ...previousState.tooltipPos, 
+                                        value: value,
+                                        time: new Date(data.labels[index]).toDateString().substring(4),
+                                        visible: !previousState.tooltipPos.visible}
+                                    }))
+                              : 
+                              this.setState({
+                                tooltipPos: {
+                                    x: x, 
+                                    y: y,
+                                    value: value,
+                                    time: new Date(data.labels[index]).toDateString().substring(4),
+                                    visible: true
+                                  }
+                                })
+            }} // end function,
+            decorator={() => {
+              return this.state.tooltipPos.visible ? <View>
+              <Svg>
+                  <Rect 
+                    x={this.state.tooltipPos.x -40}
+                    y={this.state.tooltipPos.y + 10}
+                    width="80"  
+                    height="45"
+                    fill="white"
+                    rx={5}
+                    />
+                  <TextSVG
+                    x={this.state.tooltipPos.x}
+                    y={this.state.tooltipPos.y + 25}
+                    fill="black"
+                    fontSize="12.5"
+                    textAnchor="middle">
+                    {this.state.tooltipPos.time}
+                  </TextSVG>
+                  <TextSVG
+                    x={this.state.tooltipPos.x-5}
+                    y={this.state.tooltipPos.y + 50}
+                    fill="black"
+                    fontSize="12.5"
+                    textAnchor="middle">
+                    {this.state.tooltipPos.value}°C
+                  </TextSVG>
+              </Svg>
+          </View> : null
+          }}
+          data={data}
+          width={width} // from react-native
+          height={400}
+          yAxisSuffix="°C"
+          yAxisInterval={1} // optional, defaults to 1
+          chartConfig={{
+            strokeWidth: 3,
+            backgroundGradientFromOpacity: 0,
+            backgroundGradientToOpacity: 0,
+            backgroundGradientTo: "#ffa726",
+            color: () => `rgba(141, 193, 255, 1)`,
+            labelColor: () => 'white',
+            style: {
+              borderRadius: 16
+            },
+            propsForDots: {
+              r: "2",
+            },
+            propsForBackgroundLines: {
+              strokeDasharray: "", // solid background lines with no dashes
+              opacity: 0.15
+            }
+          }}
+          formatYLabel={(y: string) => Number(y).toString()}
+          formatXLabel = {(x: string) => this.getHourMinuteSecond(new Date(x))}
+          fromZero
+          withVerticalLines={false}
+          bezier
+          style={styles.chart}
+        />
             <View style={styles.tempContainer}>
               <Feather name = "sun" size = {30} color = "white"/>
               <View style={styles.tempFirstBox}>
                 <Text style={{color: "rgba(255, 255, 255, 0.3)"}}>highest temperature</Text>
-                <Text style={{color: "rgba(255, 255, 255, 0.3)"}}>lowest temperature</Text>
               </View>
               <View style={styles.tempSecondBox}>
-                <Text style={{color:"white"}}>{Math.max(...temp_data.highestTempOfDay)}°C</Text>
-                <Text style={{color:"white"}}>{Math.min(...temp_data.highestTempOfDay)}°C</Text>
+                <Text style={{color:"white"}}>{Math.max(...temp_data.temp)}°C</Text>
               </View>          
             </View>
             <View style={styles.tempContainer}>
@@ -138,11 +249,31 @@ class Temperature extends React.Component<Props> {
                 <Text style={{color: "rgba(255, 255, 255, 0.3)"}}>lowest temperature</Text>
               </View>
               <View style={styles.tempSecondBox}>
-                <Text style={{color:"white"}}>{Math.min(...temp_data.lowestTempOfDay)}°C</Text>
+                <Text style={{color:"white"}}>{Math.min(...temp_data.temp)}°C</Text>
               </View>          
             </View>
           </ScrollView>
+          : this.renderDeviceEmptyData()}
         </View>
+      )
+    }
+    return this.renderEmptyDevice()
+  }
+  
+
+  render(){
+    let temp_data = this.getTempData()
+
+    return (
+      <LinearGradient 
+        colors={['#4F9FFF', '#002150']} 
+        style={styles.container}
+        start={{x: 0, y: 0}}
+        end={{x: 0, y: 0.5}}
+      >
+        {this.props.defaultBuilding !== undefined? 
+          this.renderData(temp_data)
+          : this.renderDeviceEmptyBuilding()}
       </LinearGradient>
     )
   }
@@ -178,5 +309,19 @@ const styles = StyleSheet.create({
   tempSecondBox: {
     height: 70,
     justifyContent: 'space-around'
+  },
+  emptyContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  emptyPrimaryText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white'
+  },
+  emptySecondaryText: {
+    fontSize: 18,
+    color: 'rgba(255, 255, 255, 0.6)'
   }
 });
